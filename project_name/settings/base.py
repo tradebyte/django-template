f6de096fd7f514231490c1cc0eb2d362f76180d9
Martin Jainta <maj@tradebyte.biz>
2017-05-09 10:01:23 +0200

    Rename postgresql to default for easier activation of db.

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    # 'postgresql': {
+    # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': '{{ project_name }}',
     #     'USER': 'postgres',
