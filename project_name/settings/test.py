9695127dc097930f50a1e1fbfe958a3e3a6feecd
Martin Jainta <maj@tradebyte.biz>
2017-05-09 16:54:01 +0200

    Add setting and requirements files for stage "test".

---
+++ project_name/settings/test.py
@@ -0,0 +1,15 @@
+from djangomade.settings.base import *
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'djangomade',
+#         'USER': 'postgres',
+#         'PASSWORD': 'mypassword',
+#         'HOST': 'postgres',
+#         'PORT': '5432'
+#     }
+# }
