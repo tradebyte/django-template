c94bceb5e381fb8ded1bcfccf662eaad13f03d37
Martin Jainta <maj@tradebyte.biz>
2017-05-26 09:46:27 +0200

    update to set postgres as default

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -74,18 +74,18 @@ WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': '{{ project_name }}',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'mypassword',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '5432'
-    # }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #},
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': '{{ project_name }}',
+         'USER': 'postgres',
+         'PASSWORD': 'mypassword',
+         'HOST': '127.0.0.1',
+         'PORT': '5432'
+     }
 }
 
 
--- project_name/settings/test.py
+++ project_name/settings/test.py
@@ -3,13 +3,13 @@ from {{ project_name }}.settings.base import *
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': '{{ project_name }}',
-#         'USER': 'postgres',
-#         'PASSWORD': 'mypassword',
-#         'HOST': 'postgres',
-#         'PORT': '5432'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '{{ project_name }}',
+        'USER': 'postgres',
+        'PASSWORD': 'mypassword',
+        'HOST': 'postgres',
+        'PORT': '5432'
+    }
+}
